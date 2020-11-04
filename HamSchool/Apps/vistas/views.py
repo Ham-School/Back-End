478f3125f770bf195e3a098c56193d7fc790bb39
KevinRVUTCH <a1117150045@utch.edu.mx>
2020-11-04 11:09:03 -0700

    test

---
+++ HamSchool/Apps/vistas/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index (request):
+    return render(request, "vistas/index.html")
+
+def login (request):
+    return render(request, "vistas/login/index.html")
+
+def article (request):
+    return render(request, "vistas/article.html")
+
+def kinder (request):
+    return render(request, "vistas/cursos/pplkinder.html")
+
+def numk (request):
+    return render(request, "vistas/cursos/numeros.html")
+
+def abck (request):
+    return render(request, "vistas/cursos/abc.html")  
+
+def animk (request):
+    return render(request, "vistas/cursos/losanimales.html")
+
+def coloresk (request):
+    return render(request, "vistas/cursos/colores.html")
+
+def finalK (request):
+    return render(request, "vistas/cursos/final.html")
+
+def primaria (request):
+    return render(request, "vistas/cursos/pplprm.html")
+
+def Alfabeto (request):
+    return render(request, "vistas/cursos/alfabetoprim.html")
+
+def Números (request):
+    return render(request, "vistas/cursos/numerosprim.html")
+
+def Colores (request):
+    return render(request, "vistas/cursos/coloresprim.html")
+
+def Figuras (request):
+    return render(request, "vistas/cursos/fgeoprim.html")    
+
+def cuerpo (request):
+    return render(request, "vistas/cursos/partescuerpoprim.html")
+
+def Pronombres (request):
+    return render(request, "vistas/cursos/pronomprim.html")  
---
+++ HamSchool/urls.py
@@ -0,0 +1,47 @@
+"""HamSchool URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from HamSchool.Apps.vistas import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index),
+    path('login/', views.login),
+    path('article.html/', views.article),
+    path('article.html/index.html', views.index),
+    path('article.html/article.html', views.article),
+    path('pplkinder.html', views.kinder),
+    path('index.html', views.index),
+    path('numeros.html', views.numk),
+    path('abc.html', views.abck),
+    path('losanimales.html', views.animk),
+    path('colores.html', views.coloresk),
+    path('final.html', views.finalK),
+    path('pplprm.html', views.primaria),
+    path('article.html/pplprm.html', views.primaria),
+    path('article.html/pplkinder.html', views.kinder),
+    path('alfabetoprim.html', views.Alfabeto),
+    path('numerosprim.html', views.Números),
+    path('coloresprim.html', views.Colores),
+    path('fgeoprim.html', views.Figuras),
+    path('partescuerpoprim.html', views.cuerpo),
+    path('pronomprim.html', views.Pronombres),
+]
+
+urlpatterns += staticfiles_urlpatterns()
